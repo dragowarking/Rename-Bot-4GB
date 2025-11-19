2302f88613ae03b2998548c14bd88a2d911c7b3f
dragowarking <dragowarking@gmail.com>
2025-11-19 10:14:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import asyncio
 from pyrogram import Client, idle
 from plugins.cb_data import app as Client2
 from config import *
@@ -8,28 +9,28 @@ pyrogram.utils.MIN_CHAT_ID = -999999999999
 pyrogram.utils.MIN_CHANNEL_ID = -100999999999999
 
 
-
-bot = Client("Renamer", bot_token=BOT_TOKEN, api_id=API_ID, api_hash=API_HASH, plugins=dict(root='plugins'))
-
-
-
-
-if STRING_SESSION:
-    apps = [Client2,bot]
-    for app in apps:
-        app.start()
-    idle()
-    for app in apps:
-        app.stop()
-    
-else:
-    bot.run()
+bot = Client(
+    "Renamer",
+    bot_token=BOT_TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH,
+    plugins=dict(root='plugins')
+)
 
 
+async def main():
+    if STRING_SESSION:
+        apps = [Client2, bot]
+        for app in apps:
+            await app.start()
+        await idle()
+        for app in apps:
+            await app.stop()
+
+    else:
+        await bot.start()
+        await idle()
+        await bot.stop()
 
 
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Back-Up Channel @JishuBotz
-# Developer @JishuDeveloper
+asyncio.run(main())
